3cef325238df1a2ef31e46605c348a419b118bf8
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-04-12 20:52:49 +0300

    Add some additions.

--- WebApplicationLearnRazorPages/Pages/About.cshtml.cs
+++ WebApplicationLearnRazorPages/Pages/About.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Bakery.Pages
+namespace WebApplicationLearnRazorPages.Pages
 {
     public class AboutModel : PageModel
     {
